0958452fc9a633aad708b901c60f445989a6e03b
Bitwarden DevOps <106330231+bitwarden-devops-bot@users.noreply.github.com>
2024-07-25 14:17:35 -0600

    Updated core, web, and key-connector versions (#277)

--- bitwarden.ps1
+++ bitwarden.ps1
@@ -28,8 +28,8 @@ $bitwardenScriptUrl = "https://func.bitwarden.com/api/dl/?app=self-host&platform
 $runScriptUrl = "https://func.bitwarden.com/api/dl/?app=self-host&platform=windows&variant=run"
 
 # Please do not create pull requests modifying the version numbers.
-$coreVersion = "2024.6.2"
-$webVersion = "2024.6.3"
+$coreVersion = "2024.7.2"
+$webVersion = "2024.7.1"
 $keyConnectorVersion = "2024.4.0"
 
 # Functions
