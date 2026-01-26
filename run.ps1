148a3355aa379402cfea910a384fc993eec8a525
Vince Grassia <593223+vgrassia@users.noreply.github.com>
2026-01-26 16:29:27 -0500

    Remove Certbot cleanup functions and logic (#457)

--- run.ps1
+++ run.ps1
@@ -70,8 +70,6 @@ function Install() {
                 "certonly{0} --standalone --noninteractive --agree-tos --preferred-challenges http " + `
                 "--email ${email} -d ${domain} --logs-dir /etc/letsencrypt/logs"
             Invoke-Expression ($certbotExp -f $qFlag)
-
-            Cleanup-Certbot
         }
     }
 
@@ -154,8 +152,6 @@ function Update-Lets-Encrypt {
             "-v ${outputDir}/letsencrypt:/etc/letsencrypt/ certbot/certbot " + `
             "renew{0} --logs-dir /etc/letsencrypt/logs" -f $qFlag
         Invoke-Expression $certbotExp
-
-        Cleanup-Certbot
     }
 }
 
@@ -166,8 +162,6 @@ function Force-Update-Lets-Encrypt {
             "-v ${outputDir}/letsencrypt:/etc/letsencrypt/ certbot/certbot " + `
             "renew{0} --logs-dir /etc/letsencrypt/logs --force-renew" -f $qFlag
         Invoke-Expression $certbotExp
-
-        Cleanup-Certbot
     }
 }
 
@@ -227,8 +221,6 @@ function Uninstall() {
     if ($purgeAction -eq "y") {
         Docker-Prune
     }
-    
-    Cleanup-Certbot
 }
 
 function Print-Environment {
@@ -263,18 +255,6 @@ function Write-Line($str) {
         Write-Host $str
     }
 }
-
-function Cleanup-Certbot {
-    # Check if the certbot image is being used by any containers
-    if ([string]::IsNullOrEmpty((docker ps -a --filter ancestor=certbot/certbot --quiet))) {
-        Write-Host "(!) " -f red -nonewline
-        $response = $( Read-Host "The [certbot/certbot] container image used by this script is no longer associated with any containers. Would you like to purge it? (y/N)" )
-        
-        if ($response.ToLower() -eq 'y') {
-            docker image rm certbot/certbot
-        }
-    }
-} 
 
 # Commands
 
